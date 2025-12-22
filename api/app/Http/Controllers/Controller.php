0abb16b4892777e65e067c745636d7c1b4060b68
C410E <caioevandro0@gmail.com>
2025-12-22 15:46:19 -0300

    primeiras configurações

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -2,7 +2,57 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Routing\Controller as BaseController;
+
+
+abstract class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function emptySend(): Response 
+    {
+        return response()->noContent(Response::HTTP_NO_CONTENT);
+    }
+    public function send($data = null, int $status = 200, array $headers = []): JsonResponse|Response
+    {
+        if ($data === null && $status === 204) {
+            return $this->emptySend();
+        }
+        $response = $data ?? [];
+
+        return response()->json([
+            'status' => $status,
+            'data' => $response
+        ], $status, $headers, empty($response) ? JSON_FORCE_OBJECT : JSON_ERROR_NONE);
+    }
+    public function sendError($data, $status): JsonResponse
+    {
+        return response()->json([
+            'status' => $status,
+            'data' => [
+                'messages' => $data,
+            ],
+        ], $status);
+    }
+    public function sendPaginatedData(LengthAwarePaginator $paginator): JsonResponse
+    {
+        return response()->json(
+            [
+                'status' => Response::HTTP_OK,
+                'meta' => [
+                    'total' => $paginator->total(),
+                    'lastPage' => $paginator->lastPage(),
+                    'perPage' => $paginator->perPage(),
+                    'currentPage' => $paginator->currentPage(),
+                ],
+                'data' => $paginator->getCollection()->toArray(),
+            ]
+        );
+    }
 }
